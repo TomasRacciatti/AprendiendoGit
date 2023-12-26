30294af5c4d796065ec299b1051f168e5b8d8a5c
TomasRacciatti <tomasracciatti@gmail.com>
2023-12-26 19:16:53 -0300

    conflicto

--- Assets/AprendiendoGit.cs
+++ Assets/AprendiendoGit.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class AprendiendoGit : MonoBehaviour
 {
 
+<<<<<<< Updated upstream
     private int prueba = 9;
+=======
+    private int prueba = 22;
+>>>>>>> Stashed changes
 
 }
